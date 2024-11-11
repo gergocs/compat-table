53e5fbf03803c1ff96b66ba9ce38f162e348d8b9
Gergo Csizi <gergocs@inf.u-szeged.hu>
2024-11-11 12:04:49 +0100

    Fix some tests which use "function" instead of "Function"

--- data-es2016plus.js
+++ data-es2016plus.js
@@ -82,7 +82,7 @@ exports.tests = [
         exec: function () {/*
           if (2 ** 3 !== 8) { return false; }
           try {
-            function ("-5 ** 2")();
+            Function ("-5 ** 2")();
           } catch (e) {
             return true;
           }
@@ -660,7 +660,7 @@ exports.tests = [
         name: 'no line break between async and function',
         exec: function () {/*
           async function a() {}
-          try { function ("async\n function a() {await 0}")(); } catch (e) { return true; }
+          try { Function ("async\n function a() {await 0}")(); } catch (e) { return true; }
         */},
         res: {
           tr: null,
@@ -794,7 +794,7 @@ exports.tests = [
         name: 'must await a value',
         exec: function () {/*
           async function a() { await Promise.resolve(); }
-          try { function ("(async function a() { await; }())")(); } catch (e) { return true; }
+          try { Function ("(async function a() { await; }())")(); } catch (e) { return true; }
         */},
         res: {
           tr: null,
@@ -859,7 +859,7 @@ exports.tests = [
         name: 'cannot await in parameters',
         exec: function () {/*
           async function a() { await Promise.resolve(); }
-          try { function ("(async function a(b = await Promise.resolve()) {}())")(); } catch (e) { return true; }
+          try { Function ("(async function a(b = await Promise.resolve()) {}())")(); } catch (e) { return true; }
         */},
         res: {
           tr: null,
@@ -1918,7 +1918,7 @@ exports.tests = [
     exec: function () {/*
       function foo(...a) {}
       try {
-        function ("function bar(...a) {'use strict';}")();
+        Function ("function bar(...a) {'use strict';}")();
       } catch (e) {
       return true;
       }
@@ -4795,7 +4795,7 @@ exports.tests = [
     subtests: [{
       name: '"globalThis" global property is global object',
       exec: function () {/*
-      var actualGlobal = function ('return this')();
+      var actualGlobal = Function ('return this')();
       actualGlobal.__system_global_test__ = 42;
       return typeof globalThis === 'object' && globalThis && globalThis === actualGlobal && !globalThis.lacksGlobalThis && globalThis.__system_global_test__ === 42;
     */},
@@ -4839,7 +4839,7 @@ exports.tests = [
     }, {
       name: '"globalThis" global property has correct property descriptor',
       exec: function () {/*
-      var actualGlobal = function ('return this')();
+      var actualGlobal = Function ('return this')();
       if (typeof globalThis !== 'object') { return false; }
       if (!('globalThis' in actualGlobal)) { return false; }
       if (Object.prototype.propertyIsEnumerable.call(actualGlobal, 'globalThis')) { return false; }
